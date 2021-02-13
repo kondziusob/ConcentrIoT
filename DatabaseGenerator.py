7a4dfe7c4d711bb31cf5f060c5f09ce4ffd7d7b5
FilipKubackiSoton <56367856+FilipKubackiSoton@users.noreply.github.com>
2021-02-13 18:42:46 +0000

    training with args

--- DatabaseGenerator.py
+++ DatabaseGenerator.py
@@ -14,8 +14,7 @@ from PIL import Image
 class DataGenerator(tf.data.Dataset):
     
     def _generator(num_samples, size = (224,224), SAMPLE_DIR = "Database/cars_train" , FG_DIR="Database/VW/tmp3", MASK_DIR = "Database/VW_Masks2", BG_DIR="Database/Backgrounds"):
-        # Opening the file
-        #time.sleep(0.03)
+        
         data_augmentation = tf.keras.Sequential([
           tf.keras.layers.experimental.preprocessing.RandomZoom(-0.3, -0.3),
           tf.keras.layers.experimental.preprocessing.RandomRotation(0.1),
@@ -27,19 +26,22 @@ class DataGenerator(tf.data.Dataset):
           RandomColorDistortion(), # colour distoriton 
           tf.keras.layers.experimental.preprocessing.RandomContrast(0.5) #contrast distortion 
         ])
-        
+        FG_DIR = FG_DIR.decode("utf-8")
         fg_list = os.listdir(FG_DIR)[:-1]
-        fg_img = [imageio.imread(os.path.join(FG_DIR, i)) for i in fg_list]          
+        fg_img = [tf.expand_dims(imageio.imread(os.path.join(FG_DIR, i)),0) for i in fg_list]          
         fg_size = len(fg_list)
         
+        MASK_DIR = MASK_DIR.decode("utf-8")
         mask_img = [np.repeat(imageio.imread(os.path.join(MASK_DIR, i))[:, :, np.newaxis], 3, axis=2)  for i in fg_list]
         
+        BG_DIR = BG_DIR.decode("utf-8")
         bg_list = os.listdir(BG_DIR)[:-1]
-        bg_img = [imageio.imread(os.path.join(BG_DIR, i)) for i in bg_list]          
+        bg_img = [tf.expand_dims(imageio.imread(os.path.join(BG_DIR, i)),0) for i in bg_list]          
         bg_size = len(bg_list)
         
-        sample_list = os.listdir(SAMPLE_DIR)[:30]
-        sample_img = [imageio.imread(os.path.join(SAMPLE_DIR, i)) for i in sample_list]          
+        SAMPLE_DIR = SAMPLE_DIR.decode("utf-8")
+        sample_list = os.listdir(SAMPLE_DIR)[:50]
+        sample_img = [tf.expand_dims(imageio.imread(os.path.join(SAMPLE_DIR, i)),0) for i in sample_list]          
         sample_size = len(sample_list)
         img_shape = size + (3,)
 
@@ -48,67 +50,51 @@ class DataGenerator(tf.data.Dataset):
             if class_index:
                 
                 fg_index = random.randint(0, fg_size-1)
-                foreground =  fg_img[fg_index]
-                """ 
-                fg_name = os.path.join(FG_DIR, fg_list[fg_index])
-                #img = np.memmap(fg_name, dtype=np.uint8, shape=(224, 224, 3))
-                
-
-                mask_name = os.path.join(MASK_DIR, fg_list[fg_index].split('.')[0] +"_mask.jpg")
-                """
-                #bg_name = os.path.join(BG_DIR, bg_list[bg_index])
-                
-                #plt.imread("Database/cars_train/00001.jpg", format='jpg')
-                
-                #foreground = cv2.imread(fg_name)
-                
-                #foreground = np.asarray(Image.open(fg_name))
-                
-                foreground = tf.expand_dims(foreground, 0)
+                foreground =  fg_img[fg_index] 
+                #foreground = tf.expand_dims(foreground, 0)
                 foreground = color_augmentation(foreground, training = True).numpy()[0,:,:,:]
                 
                 bg_index = random.randint(0, bg_size - 1)
                 background = bg_img[bg_index]
-                #background = imageio.imread(bg_name) # np.memmap(bg_name, dtype=np.uint8, shape=img_shape)
-                #background = np.asarray(Image.open(bg_name))
-                background = tf.expand_dims(background, 0)
+                
+                #background = tf.expand_dims(background, 0)
                 background = data_augmentation(background, training = True)
                 background = color_augmentation(background, training = True)    
                 background = background.numpy()[0,:,:,:]
 
-                #mask = imageio.imread(mask_name) #np.memmap(mask_name, dtype=np.uint8, shape=img_shape)
-                #mask = np.asarray(Image.open(mask_name))
                 mask = mask_img[fg_index]
-                #mask = np.repeat(mask[:, :, np.newaxis], 3, axis=2)
-
+                
                 foreground = np.multiply(mask, foreground)
                 background = np.multiply(1 - mask, background)
-                
 
                 img = np.add(foreground, background)
                 
-                
             else: 
                 img_index = random.randint(0, sample_size-1)
-                img_name =os.path.join(SAMPLE_DIR, sample_list[img_index])
-                img = sample_img[img_index]
-                #img = imageio.imread(img_name) # np.memmap(img_name, dtype=np.uint8, shape=img_shape)
-                
-                img = tf.expand_dims(img, 0)
+                img = sample_img[img_index]                
+                #img = tf.expand_dims(img, 0)
                 img = data_augmentation(img, training = True)
                 img = color_augmentation(img, training = True).numpy()[0,:,:,:]
                 
 
             yield (img, class_index,)
-    def __new__(cls,num_samples=160 ):
-        #img_shape = self.IMG_SIZE + (3, )
+    
+    def __new__(cls,num_samples, img_size, samle_dir, fg_dir, mask_dir, bg_dir):
+        img_shape = img_size + (3, )
+        
         return tf.data.Dataset.from_generator(
             cls._generator,
-            output_shapes = ((224,224,3), ()),
+            output_shapes = (img_shape, ()),
             output_types = (tf.float32, tf.int32),
-            args=(num_samples, )
+            args=(
+                num_samples, 
+                img_size, 
+                samle_dir, 
+                fg_dir, 
+                mask_dir, 
+                bg_dir
+            )
         )
-
 
 class RandomColorDistortion(tf.keras.layers.Layer):
     def __init__(self, contrast_range=[0.5, 1.5], 
@@ -127,9 +113,9 @@ class RandomColorDistortion(tf.keras.layers.Layer):
             self.brightness_delta[0], self.brightness_delta[1])
         images = tf.image.adjust_contrast(images, contrast)
         images = tf.image.adjust_brightness(images, brightness)
-        #images = tf.clip_by_value(images, 0, 1)
-        return images
 
+        return images
+    
 """
     
     
